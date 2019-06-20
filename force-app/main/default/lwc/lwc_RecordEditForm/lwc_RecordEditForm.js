1a6bfc68f7edde6c8be17553d904dfc16eb80bba
niskedia <36759620+niskedia@users.noreply.github.com>
2019-06-21 01:19:33 +0530

    lwctestpush

---
+++ force-app/main/default/lwc/lwc_RecordEditForm/lwc_RecordEditForm.js
@@ -0,0 +1,29 @@
+import { LightningElement,api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class Lwc_RecordEditForm extends LightningElement {
+
+    @api recordId;
+
+    handleReset(event) {
+        const inputFields = this.template.querySelectorAll(
+            'lightning-input-field'
+        );
+        if (inputFields) {
+            inputFields.forEach(field => {
+                field.reset();
+            });
+        }
+     }
+
+     handleError(event){
+        console.log(event.detail);
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error creating record',
+                message: event.detail.message,
+                variant: 'error',
+            }),
+        );
+    }
+}
---
+++ force-app/main/default/lwc/lwc_RecordViewForm/lwc_RecordViewForm.js
@@ -0,0 +1,29 @@
+import { LightningElement, api, wire } from 'lwc';
+/* Wire adapter to fetch record data */
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import OWNER_NAME from '@salesforce/schema/Account.Owner.Name';
+
+const FIELDSARRAY = [NAME_FIELD, OWNER_NAME];
+export default class LWC_RecordViewForm extends LightningElement {
+     /** Id of record to display. */
+    @api recordId;
+
+    /* Expose schema objects/fields to the template. */
+    accountObject = ACCOUNT_OBJECT;
+
+    /* Load Account.Name for custom rendering */
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDSARRAY })
+    record;
+
+    /** Get the Account.Name value. */
+    get nameValue() {
+        console.log("The record is",this.record);
+        return this.record.data ? getFieldValue(this.record.data, NAME_FIELD) : '';
+    }
+
+    get ownerName() {
+        return this.record.data ? getFieldValue(this.record.data, OWNER_NAME) : '';
+    }
+}
